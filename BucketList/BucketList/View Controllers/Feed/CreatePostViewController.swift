1e669ff2fdf5cf968320397db250e1f90a85b179
Ethan Andersen <AndersenEthanG@gmail.com>
2021-06-25 11:57:57 -0600

    Making post VC

--- BucketList/BucketList/View Controllers/Feed/CreatePostViewController.swift
+++ BucketList/BucketList/View Controllers/Feed/CreatePostViewController.swift
@@ -3,20 +3,33 @@
 //  BucketList
 //
 //  Created by Gavin Woffinden on 6/22/21.
+//  Heavily Forged in Digital Fire by Ethan Andersen on 6/25/21
 //
 
 import UIKit
 
 class CreatePostViewController: UIViewController {
 
+    // MARK: - Outlets
     @IBOutlet weak var postDescriptionTextField: UITextField!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    // MARK: - Lifeycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateView()
     }
     
+    
+    // MARK: - Functions
+    func updateView() {
+        
+    } // End of Function Update View
+    
+    
+    // MARK: - Actions
     @IBAction func doneButtonTapped(_ sender: Any) {
     }
     @IBAction func toDoButtonTapped(_ sender: Any) {
@@ -25,15 +38,7 @@ class CreatePostViewController: UIViewController {
         FirebaseFunctions.createPost(title: titleTextField.text!, description: postDescriptionTextField.text!, imageID: "lift", progress: false)
         print("Stuff worked")
     }
+
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    
+} // End of Class
